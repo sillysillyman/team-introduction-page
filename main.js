7cc3785672cc23556e4515f7360b83d82a253a40
sillysillyman <sillysillyman.cs@gmail.com>
2024-04-17 15:52:09 +0900

    update name to be underlined when clicked

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 $(document).ready(function () {
     $('.intro').hide();
     $('.profile-img').css('filter', 'grayscale(100%)');
+    $('.profile-name').css('text-decoration', 'none');
     $('#johnProfileImg').css('filter', 'grayscale(0%)');
+    $('#john').css('text-decoration', 'underline');
     $('#johnIntro').show();
     $('#carouselExampleIndicators').carousel({
         interval: 5000,
@@ -17,23 +19,31 @@ $('#johnProfileImg').click(() => {
     $('.intro').hide();
     $('.profile-img').css('filter', 'grayscale(100%)');
     $('#johnProfileImg').css('filter', 'grayscale(0%)');
+    $('.profile-name').css('text-decoration', 'none');
+    $('#john').css('text-decoration', 'underline');
     $('#johnIntro').toggle();
 });
 $('#davidProfileImg').click(() => {
     $('.intro').hide();
     $('.profile-img').css('filter', 'grayscale(100%)');
     $('#davidProfileImg').css('filter', 'grayscale(0%)');
+    $('.profile-name').css('text-decoration', 'none');
+    $('#david').css('text-decoration', 'underline');
     $('#davidIntro').toggle();
 });
 $('#arialProfileImg').click(() => {
     $('.intro').hide();
     $('.profile-img').css('filter', 'grayscale(100%)');
     $('#arialProfileImg').css('filter', 'grayscale(0%)');
+    $('.profile-name').css('text-decoration', 'none');
+    $('#arial').css('text-decoration', 'underline');
     $('#arialIntro').toggle();
 });
 $('#kevinProfileImg').click(() => {
     $('.intro').hide();
     $('.profile-img').css('filter', 'grayscale(100%)');
     $('#kevinProfileImg').css('filter', 'grayscale(0%)');
+    $('.profile-name').css('text-decoration', 'none');
+    $('#kevin').css('text-decoration', 'underline');
     $('#kevinIntro').toggle();
 });
