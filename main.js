2d0967d1ae2e3f6a4116d44042a68ef496b1cac5
fenec_fox <atimevil@gmail.com>
2024-04-17 22:12:30 +0900

    메인 재수정 진짜 마지막임 실수 안하기

--- main.js
+++ main.js
@@ -10,10 +10,40 @@ $(document).ready(function () {
         ride: "carousel"
     });
 });
-
-$(`#${name}Img`).click(() => {
+$('.profile-img').click(function () {
+    $('html, body').animate({
+        scrollTop: $(this).offset().top - 100
+    }, 0, 'linear');
+});
+$('#johnProfileImg').click(() => {
     $('.intro').hide();
     $('.profile-img').css('filter', 'grayscale(100%)');
-    $(`#${name}Img`).css('filter', 'grayscale(0%)');
-    $(`#${name}Intro`).toggle();
+    $('#johnProfileImg').css('filter', 'grayscale(0%)');
+    $('.profile-name').css('text-decoration', 'none');
+    $('#john').css('text-decoration', 'underline');
+    $('#johnIntro').toggle();
+});
+$('#davidProfileImg').click(() => {
+    $('.intro').hide();
+    $('.profile-img').css('filter', 'grayscale(100%)');
+    $('#davidProfileImg').css('filter', 'grayscale(0%)');
+    $('.profile-name').css('text-decoration', 'none');
+    $('#david').css('text-decoration', 'underline');
+    $('#davidIntro').toggle();
+});
+$('#arialProfileImg').click(() => {
+    $('.intro').hide();
+    $('.profile-img').css('filter', 'grayscale(100%)');
+    $('#arialProfileImg').css('filter', 'grayscale(0%)');
+    $('.profile-name').css('text-decoration', 'none');
+    $('#arial').css('text-decoration', 'underline');
+    $('#arialIntro').toggle();
+});
+$('#kevinProfileImg').click(() => {
+    $('.intro').hide();
+    $('.profile-img').css('filter', 'grayscale(100%)');
+    $('#kevinProfileImg').css('filter', 'grayscale(0%)');
+    $('.profile-name').css('text-decoration', 'none');
+    $('#kevin').css('text-decoration', 'underline');
+    $('#kevinIntro').toggle();
 });
