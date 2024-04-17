177473c90af4f278be97857382a3bbd63e5f2ca4
sillysillyman <sillysillyman.cs@gmail.com>
2024-04-17 14:03:11 +0900

    separate script from html to js

---
+++ main.js
@@ -0,0 +1,39 @@
+$(document).ready(function () {
+    $('.intro').hide();
+    $('.profile-img').css('filter', 'grayscale(100%)');
+    $('#johnProfileImg').css('filter', 'grayscale(0%)');
+    $('#johnIntro').show();
+    $('#carouselExampleIndicators').carousel({
+        interval: 5000,
+        ride: "carousel"
+    });
+});
+$('.profile-img').click(function () {
+    $('html, body').animate({
+        scrollTop: $(this).offset().top - 100
+    }, 0, 'linear');
+});
+$('#johnProfileImg').click(() => {
+    $('.intro').hide();
+    $('.profile-img').css('filter', 'grayscale(100%)');
+    $('#johnProfileImg').css('filter', 'grayscale(0%)');
+    $('#johnIntro').toggle();
+});
+$('#davidProfileImg').click(() => {
+    $('.intro').hide();
+    $('.profile-img').css('filter', 'grayscale(100%)');
+    $('#davidProfileImg').css('filter', 'grayscale(0%)');
+    $('#davidIntro').toggle();
+});
+$('#arialProfileImg').click(() => {
+    $('.intro').hide();
+    $('.profile-img').css('filter', 'grayscale(100%)');
+    $('#arialProfileImg').css('filter', 'grayscale(0%)');
+    $('#arialIntro').toggle();
+});
+$('#kevinProfileImg').click(() => {
+    $('.intro').hide();
+    $('.profile-img').css('filter', 'grayscale(100%)');
+    $('#kevinProfileImg').css('filter', 'grayscale(0%)');
+    $('#kevinIntro').toggle();
+});
